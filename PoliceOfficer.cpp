0e63ad029e72c68c69a393e9fc4ef487b29ae383
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-04-09 17:45:17 -0400

    a

--- PoliceOfficer.cpp
+++ PoliceOfficer.cpp
@@ -1,5 +1,41 @@
 #include "PoliceOfficer.h"
 
-PoliceOfficer::PoliceOfficer(string n, string b)
+PoliceOfficer::PoliceOfficer(string name, string badgenumber): name(name), badgenumber(badgenumber)
 {
+
+}
+
+ParkingTicket PoliceOfficer::inspectCar(ParkedCar* parkedCar, ParkingMeter* parkingMeter)
+{
+	return ParkingTicket();
+}
+
+ParkingTicket::ParkingTicket(string carDetails, float fineAmount, string officerName, string officerBadge): carDetails(carDetails), fineAmount(fineAmount), officerName(officerName), officerBadge(officerBadge)
+{
+
+}
+
+ParkedCar::ParkedCar(string make, string model, string color, string licsenceNumber, int minutesParked): make(make), model(model), color(color), licsenceNumber(licsenceNumber), minutesParked(minutesParked)
+{
+
+}
+
+string ParkedCar::getDetails()
+{
+	return string();
+}
+
+ParkingMeter::ParkingMeter(int purchasedTime): purchasedTime(purchasedTime)
+{
+
+}
+
+int ParkingMeter::getPurchasedTime()
+{
+	return 0;
+}
+
+string ParkingTicket::getDetails()
+{
+	return string();
 }
--- PoliceOfficer.h
+++ PoliceOfficer.h
@@ -5,17 +5,20 @@ using namespace std;
 
 class ParkedCar {
 public:
-	string make;
-	string model;
-	string color;
-	string licsenceNumber;
-	int minutesParked;
-
+	string make = "Toyota";
+	string model = "Camry";
+	string color = "red";
+	string licsenceNumber = "XYZ123";
+	int minutesParked = 30;
+	ParkedCar(string make, string model, string color, string licsenceNumber, int minutesParked);
+	string getDetails();
 };
 
 class ParkingMeter {
 public:
-	int purchasedTime;
+	int purchasedTime = 40;
+	ParkingMeter(int purchasedTime);
+	int getPurchasedTime();
 };
 
 class ParkingTicket {
@@ -24,14 +27,22 @@ public:
 	float fineAmount;
 	string officerName;
 	string officerBadge;
+
+	ParkingTicket(string carDetails, float fineAmount, string officerName, string officerBadge);
+
+	string getDetails();
 };
 
 class PoliceOfficer {
-
+private:
+	ParkedCar* parkedCar;
+	ParkingMeter* parkingMeter;
 public: 
-	string name;
-	string badgenumber;
+	string name = "Jane Smith";
+	string badgenumber="1234";
 
-	PoliceOfficer(string n, string b);
+	PoliceOfficer(string name, string badgenumber);
+
+	ParkingTicket inspectCar(ParkedCar* parkedCar, ParkingMeter* parkingMeter);
 };
 
