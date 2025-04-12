cac6f9df58778b21d619d2b0b7d0a36b29dcfcc0
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-04-12 14:13:35 -0400

    a

--- Intro to Classes Project.cpp
+++ Intro to Classes Project.cpp
@@ -21,6 +21,20 @@ int main()
     ParkedCar* car3 = new ParkedCar("Ford", "Mustang", "Black", "LMN456", 190);
     ParkingMeter* Meter3 = new ParkingMeter(60);
     PoliceOfficer Officer3(Name3, badge3, car3, Meter3);
+
+    ParkedCar* car4 = new ParkedCar("Honda", "Civic", "Blue", "OMO987", 70);
+    ParkingMeter* Meter4 = new ParkingMeter(120);
+   
+    ParkedCar* car5 = new ParkedCar("Nissan", "Rogue", "Red", "CFF447", 100);
+    ParkingMeter* Meter5 = new ParkingMeter(70);
+   
+    ParkedCar* car6 = new ParkedCar("Honda", "Accord", "Blue", "ABC987", 240);
+    ParkingMeter* Meter6 = new ParkingMeter(60);
+    string Name4 = "Chief Micheal";
+    string badge4 = "3141";
+    PoliceOfficer Officer4(Name4, badge4, car4, Meter4);
+                  Officer4.compareTimes(car5, Meter5);
+                  Officer4.compareTimes(car6, Meter6);
 }
 
 
